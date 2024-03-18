23e0e2f8549b19ff42ab7257e48026f1ea15c286
Joao <jmmsp2003@hotmail.com>
2024-03-18 16:55:16 -0300

    prototipo pronto

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -1,21 +1,43 @@
-ScrollReveal().reveal(".bannerGradient", {
+ScrollReveal().reveal(".logo-full", {
   duration: 2000,
   distance: "100px",
   easing: "ease",
   reset: true,
 });
+
+ScrollReveal().reveal(".bigCardContainer", {
+  duration: 2000,
+  distance: "100px",
+  easing: "ease",
+});
+
 ScrollReveal().reveal(".cardsContainer", {
   duration: 2000,
-  distance: "5px",
+  distance: "100px",
   easing: "ease",
-  reset: false,
 });
+
+ScrollReveal().reveal(".teamImage", {
+  duration: 2000,
+  origin: "left",
+  distance: "100px",
+  easing: "ease",
+});
+
+ScrollReveal().reveal(".bannerImage__image", {
+  duration: 2000,
+  origin: "right",
+  distance: "100px",
+  easing: "ease",
+});
+
 ScrollReveal().reveal(".contactDestac__image", {
   duration: 1000,
   distance: "10px",
   easing: "ease",
   reset: true,
 });
+
 ScrollReveal().reveal(".contactDestac__image", {
   origin: "left",
   distance: "100px",
@@ -23,6 +45,7 @@ ScrollReveal().reveal(".contactDestac__image", {
   easing: "ease",
   reset: false,
 });
+
 ScrollReveal().reveal(".companyName", {
   origin: "right",
   distance: "100px",
@@ -30,4 +53,3 @@ ScrollReveal().reveal(".companyName", {
   easing: "ease",
   reset: false,
 });
-
