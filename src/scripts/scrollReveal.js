48acda8a110fd6a9db3759fd0a8886be8db5c897
Joao <jmmsp2003@hotmail.com>
2024-07-31 18:52:28 -0300

    feat: add scroll reveal

---
+++ src/scripts/scrollReveal.js
@@ -0,0 +1,55 @@
+ScrollReveal().reveal(".logo-full", {
+  duration: 2000,
+  distance: "100px",
+  easing: "ease",
+  reset: true,
+});
+
+ScrollReveal().reveal(".bigCardContainer", {
+  duration: 2000,
+  distance: "100px",
+  easing: "ease",
+});
+
+ScrollReveal().reveal(".cardsContainer", {
+  duration: 2000,
+  distance: "100px",
+  easing: "ease",
+});
+
+ScrollReveal().reveal(".teamImage", {
+  duration: 2000,
+  origin: "left",
+  distance: "100px",
+  easing: "ease",
+});
+
+ScrollReveal().reveal(".bannerImage__image", {
+  duration: 2000,
+  origin: "right",
+  distance: "100px",
+  easing: "ease",
+});
+
+ScrollReveal().reveal(".contactDestac__image", {
+  duration: 1000,
+  distance: "10px",
+  easing: "ease",
+  reset: true,
+});
+
+ScrollReveal().reveal(".contactDestac__image", {
+  origin: "left",
+  distance: "100px",
+  duration: 1000,
+  easing: "ease",
+  reset: false,
+});
+
+ScrollReveal().reveal(".companyName", {
+  origin: "right",
+  distance: "100px",
+  duration: 1000,
+  easing: "ease",
+  reset: false,
+});
